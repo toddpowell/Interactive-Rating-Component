e5b79c69c6c5c79e07f39456331b8c6b733ec905
Todd Powell <toddwpowell@gmail.com>
2023-09-15 12:21:32 -0600

    Add base colors and import Overpass font

--- src/components/InteractiveRating.tsx
+++ src/components/InteractiveRating.tsx
@@ -1,8 +1,13 @@
 // import React from "react";
 import "./InteractiveRating.css";
+<style>@import url('https://fonts.google.com/specimen/Overpass');</style>;
 
 const InteractiveRating = () => {
-  return <div>InteractiveRating</div>;
+  return (
+    <>
+      <div>InteractiveRating</div>
+    </>
+  );
 };
 
 export default InteractiveRating;
