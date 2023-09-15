4b5084622d8a4b6e70c14f06c6ef0922753489a5
Todd Powell <toddwpowell@gmail.com>
2023-09-15 11:52:34 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import InteractiveRating from "./components/InteractiveRating";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <InteractiveRating />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/InteractiveRating.tsx
@@ -0,0 +1,8 @@
+// import React from "react";
+import "./InteractiveRating.css";
+
+const InteractiveRating = () => {
+  return <div>InteractiveRating</div>;
+};
+
+export default InteractiveRating;
