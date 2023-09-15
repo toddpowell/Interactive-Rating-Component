9661364ef35dd7bf7c10223967376592295f6c1b
Todd Powell <toddwpowell@gmail.com>
2023-09-15 15:22:20 -0600

    Add circles and spacing, various css changes

--- src/components/InteractiveRating.tsx
+++ src/components/InteractiveRating.tsx
@@ -5,7 +5,25 @@ import "./InteractiveRating.css";
 const InteractiveRating = () => {
   return (
     <>
-      <div>InteractiveRating</div>
+      <div className="interactive-rating-component">
+        <div className="rating-panel">
+          <div className="stars">*</div>
+          <h1>How did we do?</h1>
+          <p>
+            Please let us know how we did with your support request. All
+            feedback is appreciated to help us improve our offering!
+          </p>
+        </div>
+        <div className="ratings">
+          <div className="rating">1</div>
+          <div className="rating">2</div>
+          <div className="rating">3</div>
+          <div className="rating">4</div>
+          <div className="rating">5</div>
+        </div>
+        <button className="submit">SUBMIT</button>
+        <div className="thank-you-panel"></div>
+      </div>
     </>
   );
 };
